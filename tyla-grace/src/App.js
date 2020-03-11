8b48a11f57260438372cc0a430a73504563b3c2a
Deondre Jemison <jemisodd@gmatc.matc.edu>
2020-03-10 22:54:34 -0500

    Added NavBar

--- tyla-grace/src/App.js
+++ tyla-grace/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Drawer, Header, Content, Navigation }from 'react-mdl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* Uses a header that scrolls with the text, rather than staying locked at the top */
+      <div className="demo-big-content">
+          <Layout>
+              <Header title="Tyla Grace" scroll>
+                  <Navigation>
+                      <a href="/books.html">Books</a>
+                      <a href="/events.html">Events</a>
+                      <a href="/home.html">Home</a>
+                      <a href="/contact.html">Contact</a>
+                  </Navigation>
+              </Header>
+              <Drawer title="Tyla Grace">
+                  <Navigation>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+              </Content>
+          </Layout>
+      </div>
   );
 }
 
